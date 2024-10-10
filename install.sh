1e76df762eb6123e92683469aba711504029efdf
Dragin <dragin@dragin.xyz>
2024-10-10 18:45:02 -0500

    Improve install.sh for Debian

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-python -m venv ./venv
-source ./venv/bin/activate
-pip install -r requirements.txt
+sudo apt-get update && sudo apt-get upgrade
+&& dpkg --set-selections < packages.txt
+&& sudo apt-get dselect-upgrade
+&& python3 -m venv ./venv
+&& source ./venv/bin/activate
+&& pip install -r requirements.txt
 deactivate
