b088ebd98490dad0d1c7ee73c2eb2884ccc3a7d3
Dragin <dragin@dragin.xyz>
2024-10-11 20:33:01 -0500

    Improve install script PACKAGES WILL NOT WORK UNTIL UPDATED FROM THE FIELD PC

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade && dpkg --set-selections < packages.txt && sudo apt-get dselect-upgrade && python3 -m venv ./venv && source ./venv/bin/activate && pip install -r requirements.txt; deactivate
+sudo apt-get update &&
+sudo apt-get upgrade -y &&
+while read -r p; do sudo apt-get install -y "$p"; done < packages.txt &&
+python3 -m venv ./venv &&
+source ./venv/bin/activate &&
+pip install -r requirements.txt;
+deactivate
