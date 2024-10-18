d04f4c47c918ba021d0484fc6e8601f39eb2712b
Dragin <dragin@dragin.xyz>
2024-10-18 09:59:47 -0500

    Add error handling if credentials are invalid

--- signin.py
+++ signin.py
@@ -31,7 +31,14 @@ def init_auth():
         creds = Credentials.from_authorized_user_file("token.json")
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except google.auth.exceptions.RefreshError as error:
+                print(error)
+                print("Invalid credentials, opening CMD")
+                os.system("cmd.exe /c start cmd")
+                os.system("gnome-terminal")
+                quit()
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", SCOPES
